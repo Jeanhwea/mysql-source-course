47d1325c4246b8ebafcd6829224f558b99b22290
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-05-08 11:49:52 +0800

    add: 调整 PID 文件名称

--- scripts/sys-print-threads.sh
+++ scripts/sys-print-threads.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export MYSQL_HOME=${MYSQL_HOME:-/opt/mysql}
-export MYSQL_PID=$(cat $MYSQL_HOME/mysqld.pid)
+export MYSQLD_PID=$(cat $MYSQL_HOME/mysqld.pid)
 
-ps -T -p $MYSQL_PID
+ps -T -p $MYSQLD_PID
--- scripts/sys-top-threads.sh
+++ scripts/sys-top-threads.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export MYSQL_HOME=${MYSQL_HOME:-/opt/mysql}
-export MYSQL_PID=$(cat $MYSQL_HOME/mysqld.pid)
+export MYSQLD_PID=$(cat $MYSQL_HOME/mysqld.pid)
 
-top -H -p $MYSQL_PID
+top -H -p $MYSQLD_PID
