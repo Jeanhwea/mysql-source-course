0bd8186bcfd602e4abdc0c08cf7fb54674937faf
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-05-09 08:21:23 +0800

    update: 添加 lldb 启动常见配置的注释

--- scripts/dbg-start-lldb.sh
+++ scripts/dbg-start-lldb.sh
@@ -7,6 +7,17 @@ if [ -f $MYSQL_HOME/mysqld.pid ]; then
     exit
 fi
 
+# set args
+# (lldb) settings set target.run-args --gdb
+
+# show args
+# (lldb) settings show target.run-args
+
+# set envs
+# (lldb) settings set target.env-vars DEBUG=1
+# (lldb) set se target.env-vars DEBUG=1
+# (lldb) env DEBUG=1
+
 lldb -o "b main" \
      -o "r" \
      -- mysqld --gdb
