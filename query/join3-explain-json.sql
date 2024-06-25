e65e5b54ccab7e1eb588a8611f39cb8c8c2e914b
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-06-25 09:11:24 +0800

    add query code

---
+++ query/join3-explain-json.sql
@@ -0,0 +1,12 @@
+use employees;
+
+explain format=json
+
+select
+  e.first_name, e.last_name, a.from_date, a.to_date
+from
+  dept_manager a join departments d join employees e
+where
+  a.dept_no = d.dept_no and a.emp_no = e.emp_no and d.dept_no = 'd001'\G
+
+-- show warnings\G
