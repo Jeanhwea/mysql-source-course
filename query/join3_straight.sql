51af18d546107c60ed6f86275ab2501f90489386
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-06-29 08:01:43 +0800

    xx

---
+++ query/join3_straight.sql
@@ -0,0 +1,6 @@
+select /*+ rtc */
+  e.first_name, e.last_name, a.from_date, a.to_date
+from
+  dept_manager a straight_join departments d straight_join employees e
+where
+  a.dept_no = d.dept_no and a.emp_no = e.emp_no and d.dept_no = 'd001';
