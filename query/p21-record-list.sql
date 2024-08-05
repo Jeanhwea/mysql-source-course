23ea41352fc20bacdc570216485e34c74b941b7c
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-08-05 09:14:43 +0800

    add commit codes

---
+++ query/p21-record-list.sql
@@ -0,0 +1,11 @@
+create table t (k int primary key, v char(4));
+
+insert into t values(1, 'aaaa');
+insert into t values(2, 'bbbb');
+insert into t values(3, 'cccc');
+insert into t values(4, 'dddd');
+insert into t values(5, 'eeee');
+insert into t values(6, 'ffff');
+insert into t values(7, 'gggg');
+insert into t values(8, 'hhhh');
+insert into t values(9, 'iiii');
