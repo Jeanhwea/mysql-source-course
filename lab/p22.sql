e07eed7363e8278930bec86a40339ba837f248ba
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-08-12 19:25:43 +0800

    add code

--- lab/p22.sql
+++ lab/p22.sql
@@ -1,6 +1,9 @@
 create table t1 (
   k1 int,
-  c1 varchar(4),
-  c2 varchar(4),
+  c1 varchar(8),
+  c2 varchar(8),
   primary key (k1)
 );
+
+-- insert into t1 (k1, c1, c2) values (1, 'r1c1', 'r1c2');
+-- insert into t1 (k1, c1, c2) values (2, 'r2c1', 'r2c2');
