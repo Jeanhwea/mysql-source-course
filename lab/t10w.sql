b034788166f93a0c5238fbf6e686885e6618c510
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-08-13 11:52:12 +0800

    update mock 10w script

--- lab/t10w.sql
+++ lab/t10w.sql
@@ -1,26 +1,28 @@
 use rtc;
 
-create table t (
+drop table if exists t10w;
+
+create table t10w (
   k1 int,
   c1 varchar(8),
   c2 varchar(8),
   primary key (k1)
 );
 
-delimiter ;;
+delimiter //
 
-create procedure mockdata ()
+create procedure mock10w ()
 begin
 declare
   i int;
   set i = 1;
   while (i <= 100000)
-  do insert into t
+  do insert into t10w
     values (i, i, i);
   set i = i + 1;
 end while;
-end;;
+end //
 
 delimiter ;
 
-call mockdata();
+call mock10w();
