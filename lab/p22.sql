7360972ccf7ca987ea5a2991ab2bb6ab31c853b7
Jinghui Hu <hujinghui@buaa.edu.cn>
2024-08-13 08:22:56 +0800

    update

--- lab/p22.sql
+++ lab/p22.sql
@@ -5,5 +5,5 @@ create table t1 (
   primary key (k1)
 );
 
--- insert into t1 (k1, c1, c2) values (1, 'r1c1', 'r1c2');
--- insert into t1 (k1, c1, c2) values (2, 'r2c1', 'r2c2');
+insert into t1 (k1, c1, c2) values (1, 'r1c1', 'r1c2');
+insert into t1 (k1, c1, c2) values (2, 'r2c1', 'r2c2');
